095adaf83b996fa079fcb7670d84f74b06798476
Mateus_Nobre <mateusnobresilva@gmail.com>
2021-11-02 18:23:35 -0300

    FEATURE - add  sessions

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <html>
 
 <head>
@@ -33,13 +34,23 @@
             Login
           </div>
           <div class="card-body">
-            <form action="http://localhost/app_help_desk/valida_login.php">
+            <form action="http://localhost/app_help_desk/valida_login.php" method="post">
               <div class="form-group">
                 <input type="email" name="email" class="form-control" placeholder="E-mail">
               </div>
               <div class="form-group">
                 <input type="password" name="senha" class="form-control" placeholder="Senha">
               </div>
+
+
+              <?php if (isset($_GET['login']) && $_GET['login'] == 'erro') { ?>
+
+                <div class="text-danger">
+                  Usuário ou senha inválidos
+                </div>
+
+              <?php } ?>
+
               <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
             </form>
           </div>
--- valida_login.php
+++ valida_login.php
@@ -1,2 +1,58 @@
 <?php
-echo "Oi";
+
+session_start();
+
+$_SESSION['autenticado'] = 'Oi, sou um valor de sessão!';
+
+print_r($_SESSION);
+echo '<hr />';
+
+// print_r($_GET);
+
+// echo $_GET['email'];
+// echo $_GET['senha'];
+
+
+$usuarios_app = array(
+    array('email' => 'mateus@gmail.com', 'senha' => '123456'),
+    array('email' => 'admin@admin.com', 'senha' => '123456'),
+);
+
+$usuario_autenticado = false;
+
+// echo '<pre>';
+// print_r($usuarios_app);
+// echo '</pre>';
+
+foreach ($usuarios_app as $user) {
+
+
+
+    if ($user['email'] ==  $_POST['email']  &&  $user['senha'] ==  $_POST['senha']) {
+        $usuario_autenticado = true;
+    }
+
+
+
+    // echo '<hr>';
+    // // print_r($user);
+    // echo '<hr>';
+}
+
+if ($usuario_autenticado) {
+    echo 'Usuário autenticado';
+    $_SESSION['autenticado'] = 'SIM';
+} else {
+    $_SESSION['autenticado'] = 'NAO';
+    //  echo 'Erro na autenticação do usuário';
+    header('Location: index.php?login=erro');
+}
+
+
+// print_r($_POST);
+
+// echo '<br />';
+
+// echo $_POST['email'];
+// echo '<br />';
+// echo $_POST['senha'];
